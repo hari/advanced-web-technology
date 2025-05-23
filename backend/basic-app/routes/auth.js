9daf9b7e95184baf0c1688ad6479e22a8ed21e5d
Hari Lamichhane <onwithhari@gmail.com>
2025-05-23 20:26:54 +0545

    add rate limiter

--- backend/basic-app/routes/auth.js
+++ backend/basic-app/routes/auth.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth');
+const rateLimit = require('express-rate-limit');
+
+const loginLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 5,
+  message: 'Too many login attempts, please try again later.',
+});
 
 router.get('/login', authController.getLogin);
-router.post('/login', authController.postLogin);
+router.post('/login', loginLimiter, authController.postLogin);
 router.get('/register', authController.getRegister);
 router.post('/register', authController.postRegister);
 router.get('/logout', authController.logout);
